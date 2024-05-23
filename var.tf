0e68c6fbc8ed55fdfa1e48a406d627d1204666d4
mojilearns <oyedelemojisola@gmail.com>
2024-05-23 19:01:07 +0100

    created local.tf and var.tf

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "owner" {
+  description = "name of dynamic vm project"
+  type = string
+  default = "solar"
+}
+variable "environment" {
+  description = "name of dynamic environment"
+  type = string
+  default = "dynamic"
+}
