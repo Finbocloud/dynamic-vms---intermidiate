24224492aa1fd7ba078a0b78ad9f4bce4bdff165
sargeene <sargeene@gmail.com>
2024-07-10 19:12:52 +0100

    updated networking

--- nsgvm.tf
+++ nsgvm.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "azurerm_network_security_group" "this_vm_nsg" {
   name                = "acceptanceTestSecurityGroup1"
   location            = azurerm_resource_group.this_rg.location
@@ -40,4 +40,3 @@ resource "azurerm_subnet_network_security_group_association" "this_vm_nsg_associ
   subnet_id                 = azurerm_subnet.this_subnet.id
   network_security_group_id = azurerm_network_security_group.this_vm_nsg.id
 }
-*/
